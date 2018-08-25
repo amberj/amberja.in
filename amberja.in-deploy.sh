2d1c8c3cc16cc30971eb66180e3e6aa9d4b81637
Amber Jain <i.amber.jain@gmail.com>
2018-08-26 01:09:55 +0530

    adding basic deployment shell script

---
+++ amberja.in-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+simple_blue_echo() {
+    echo -e "\x1b[1;34m$MESSAGE\e[0m"
+}
+
+MESSAGE="Running: 'rm *.html'" ; simple_blue_echo
+rm  *.html
+
+# Create HTML file that is needed by Google Search Console for verification of site ownership:
+echo "google-site-verification: google419a0be1d1b0767d.html" > google419a0be1d1b0767d.html
+
+MESSAGE="Running: 'rm blog/*.html'" ; simple_blue_echo
+rm blog/*.html
+
+MESSAGE="Running: 'git pull'" ; simple_blue_echo
+git pull
+
+MESSAGE="Running: 'make'" ; simple_blue_echo
+make
